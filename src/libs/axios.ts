400965bdd50b44445c2d0b6e800943ff1a4a7ec8
csarchioni <csarchioni@ater.gob.ar>
2025-05-16 13:17:57 -0300

    fix de variables de entorno

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -4,7 +4,7 @@ import { environment } from "./environment";
 
 
 const confiAxios = axios.create({
-    baseURL: environment.apiUrlLocal,
+    baseURL: environment.apiUrl,
     withCredentials: true,    
 })
 
